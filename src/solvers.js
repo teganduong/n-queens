ba4c4f5e09c391813927f2cce5eb17c7872b314d
teganduong <teganduong@gmail.com>
2016-03-10 20:03:54 -0800

    Complete countNRooksSolutions to pass spec

--- src/solvers.js
+++ src/solvers.js
@@ -18,92 +18,43 @@ window.findNRooksSolution = function(n) {
   
   var board = new Board({ n: n });
 
-  var r, c;
-
-  r = 0;
-  c = 0;
-
-  var size = n;
-
-  // place an intitial marker
-  board.togglePiece(r, c);
-  // move column position one to right
-  c++;
-
-  console.log('board1: ', board);
-  console.log('start position after placing first piece: row: ', r, 'col: ',c);
-  debugger;
-  
-  // start testing
-  // loop through columns
-  for (c; c < size; c++) {
-    // check for row conflict
-    // if row conflict..
-    if (board.hasRowConflictAt(r)) {
-      // move to new row and start again
-      r++;
-      continue;
-    }
-    // if no row conflict, check for column conflict.
-    // if column conflict...
-    if (board.hasRowConflictAt(c)) {
-      // start again
-      continue;
-    }
-    // if no column conflict...
-    // set location to 1
-    board.togglePiece(r, c); 
-    // start again
-    continue;
-  }
-
-  find();
-
-
-
-
-
-
-
-
-
-
-
-
-
-  // var solution = function(n) {
-  //    this.makeEmptyMatrix(n);
-
-  // var makeAllcombinations = function(n) {
-  //   var positions = [1, 1, 1, 1];
-  //   var sequences = [];
-
-  //   var makeSequences = function(sequencesToGo, results) {
-  //     if (sequencesToGo === 0) {
-  //       return sequences.push(results);
-  //     }
-
-  //     for (var i = 0; i < positions.length; i++) {
-  //       var currentPosition = position[i];
-  //       makeSequences(sequencesToGo - 1, results.concat(currentPosition));
-  //     }
-  //   };
-  //   makeSequences(n, []);
-  //   return sequences;
-  // };
-
-  // allCombinations = makeSequences(n);
-  // debugger;
-  // console.log(allCombinations);
-
-
   console.log('Single solution for ' + n + ' rooks:', JSON.stringify(solution));
   return solution;
 };
 
 // return the number of nxn chessboards that exist, with n rooks placed such that none of them can attack each other
 window.countNRooksSolutions = function(n) {
-  var solutionCount = undefined; //fixme
+  var solutionCount = 0;
+  var board = new Board({ n: n });
+
+  // create helper function to find a rooks solution
+  var findSolution = function(row) {
+    // base case: if end of all rows (at last row)
+    if (row === n) {
+      // increment solutionCount
+      solutionCount++;
+      // return (stop)
+      return;
+    }
+
+    // iterate over possible decisions (each spot on board)
+    for (var i = 0; i < n; i++) {
+      // place a piece using toggle
+      board.togglePiece(row, i);
+      // check if placement of piece has any conflicts
+      if (!board.hasAnyRooksConflicts()) {
+        // if not, recurse findSolution w/ remaining rows
+        findSolution(row + 1);
+      }
+
+      // else (if piece placement has rook conflicts)
+      // unplace (untoggle) piece
+      board.togglePiece(row, i);
+    }
+  };
+
+  // implement findSolution starting w/ row 0
+  findSolution(0);
 
   console.log('Number of solutions for ' + n + ' rooks:', solutionCount);
   return solutionCount;
